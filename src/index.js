1f84815966ea2d2b60ebb19e3756ab7c05ead925
TeHanna <68251610+TeHanna@users.noreply.github.com>
2021-08-19 02:07:45 +0300

    feat: 'change index.js'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+    return array.sort((a, b) => b - a).pop(); 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+    return array.sort((a, b) => a - b).pop();
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) return 0;
+    let sumArr = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumArr = sumArr + array[i];
+    }
+    let avArr = sumArr/array.length;
+    return avArr;
 }
